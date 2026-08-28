033f3b84b17f00c62e2e12572774a6795a19bd63
PedroJrSuarez <pedrojsuarez3005@gmail.com>
2026-08-28 14:47:27 -0500

    El calculo de datos mas el historial de registros

--- app/src/main/java/com/maza/lab02_dm/Tarea.kt
+++ app/src/main/java/com/maza/lab02_dm/Tarea.kt
@@ -12,11 +12,14 @@ data class RegistroVehiculo (
 var limiteVehiculos: Int = 0
 var listaVehiculos = mutableListOf<RegistroVehiculo>()
 
+val historialVisitas = mutableMapOf<String, Int>()
+
 
 fun iniciarProcesamiento(cantidad: Int) {
     if (cantidad > 0) {
         limiteVehiculos = cantidad
         listaVehiculos.clear()
+        historialVisitas.clear()
     }
 }
 
@@ -33,6 +36,47 @@ fun registrarVehiculo(placa: String, tipo: String, horasIngresadas: Int, cliente
             esFrecuente = esFrecuente
         )
 
+        val montoAPagar = calcularPago(nuevoRegistro)
+
         listaVehiculos.add(nuevoRegistro)
     }
+}
+
+
+fun calculcularSiEsFrecuente(placa: String): Boolean{
+    val visistasAnteriores = historialVisitas.getOrDefault(placa, 0)
+
+    val visitasActuales = visistasAnteriores + 1
+    historialVisitas[placa] = visitasActuales
+
+    return visitasActuales > 1
+}
+
+fun calcularPago(vehiculo: RegistroVehiculo): Double {
+
+    val tarifaBase = when (vehiculo.tipo.lowercase()){
+        "moto" -> 2.0
+        "auto" -> 4.0
+        "camioneta" -> 10.0
+        else -> 0.0
+    }
+
+    var subtotal = 0.0
+    val horas = vehiculo.horas
+
+    val horasNormales = if (horas > 2) 2 else horas
+    subtotal += horasNormales * tarifaBase
+
+    if (horas > 5) {
+        val horascon50 = horas - 5
+        subtotal += horascon50 * (tarifaBase * 1.50)
+    }
+
+    if (vehiculo.esFrecuente) {
+        subtotal = subtotal * 0.90
+    }
+
+    vehiculo.totalPagado = subtotal
+
+    return subtotal
 }
