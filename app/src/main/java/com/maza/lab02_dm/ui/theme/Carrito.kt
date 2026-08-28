139dcfcb8f83e44ada4a3f4f23171d44cb0ab119
DMaza15 <dario.maza@tecsup.edu.pe>
2026-08-27 21:58:01 -0500

    Proyecto inicial con menú del carrito

---
+++ app/src/main/java/com/maza/lab02_dm/ui/theme/Carrito.kt
@@ -0,0 +1,7 @@
+package com.maza.lab02_dm.ui.theme
+
+fun main () {
+    println("==========================================")
+    println("  CARRITO DE COMPRAS - TIENDA TECSUP  ")
+    println("==========================================")
+}
