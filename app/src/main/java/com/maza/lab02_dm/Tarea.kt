da5fd7ba14c7e5d3d3b3af7073b90b3d7300535e
PedroJrSuarez <pedrojsuarez3005@gmail.com>
2026-08-28 15:05:28 -0500

    MostrarResultados

--- app/src/main/java/com/maza/lab02_dm/Tarea.kt
+++ app/src/main/java/com/maza/lab02_dm/Tarea.kt
@@ -28,6 +28,8 @@ fun registrarVehiculo(placa: String, tipo: String, horasIngresadas: Int, cliente
 
         val horasReales = if (horasIngresadas < 1) 1 else horasIngresadas
 
+        val esFrecuenteCalculado = calculcularSiEsFrecuente(placa)
+
         val nuevoRegistro = RegistroVehiculo(
             placa = placa,
             tipo = tipo,
@@ -36,14 +38,17 @@ fun registrarVehiculo(placa: String, tipo: String, horasIngresadas: Int, cliente
             esFrecuente = esFrecuente
         )
 
-        val montoAPagar = calcularPago(nuevoRegistro)
-
+        calcularPago(nuevoRegistro)
         listaVehiculos.add(nuevoRegistro)
+
+        if (listaVehiculos.size == listaVehiculos){
+            mostrarVehiculosRegistrados()
+        }
     }
 }
 
 
-fun calculcularSiEsFrecuente(placa: String): Boolean{
+fun calcularSiEsFrecuente(placa: String): Boolean{
     val visistasAnteriores = historialVisitas.getOrDefault(placa, 0)
 
     val visitasActuales = visistasAnteriores + 1
@@ -67,6 +72,11 @@ fun calcularPago(vehiculo: RegistroVehiculo): Double {
     val horasNormales = if (horas > 2) 2 else horas
     subtotal += horasNormales * tarifaBase
 
+    if (horas > 2) {
+        val horasCon20 = if (horas > 5) 3 else (horas - 2)
+        subtotal += horasCon20 * (tarifaBase * 1.20)
+    }
+
     if (horas > 5) {
         val horascon50 = horas - 5
         subtotal += horascon50 * (tarifaBase * 1.50)
@@ -79,4 +89,22 @@ fun calcularPago(vehiculo: RegistroVehiculo): Double {
     vehiculo.totalPagado = subtotal
 
     return subtotal
+}
+
+fun mostrarVehiculosRegistrados(){
+    println("=== LISTA DE VEHÍCULOS REGISTRADOS ===")
+    for ((index, vehiculo) in listaVehiculos.withIndex()) {
+        val datos = """
+            Vehículo ${index + 1}:
+           -Placa: ${vehiculo.placa}
+           -Cliente: ${vehiculo.cliente}
+           -Tipo: ${vehiculo.tipo}
+           -Horas: ${vehiculo.horas}
+           -Frecuente: ${if (vehiculo.esFrecuente) "Sí (10% Dscto) else "No""}
+           -Total a Pagar: S/ ${String.format("%.2f", vehiculo.totalPagado)}
+           ----------------------------------
+       """.trimIndent()
+
+       println(datos)
+    }
 }
